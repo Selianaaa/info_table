58e8569df495e412bbcd8ab983cc35fdc88b666d
Seliana <cany96@bk.ru>
2019-09-10 23:46:57 +0300

    table added

--- info_table.js
+++ info_table.js
@@ -1,6 +1,6 @@
 //  TODO: add jquery with autocomplete
 
-const url = "http://www.filltext.com/?rows=1&id={number|1000}\
+const url = "http://www.filltext.com/?rows=3&id={number|1000}\
 &firstName={firstName}&delay=3&lastName={lastName}\
 &email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}\
 &description={lorem|32}";
@@ -9,15 +9,15 @@ new Vue({
   el: '#info-table',
   data() {
     return {
-      events: null,
+      persons: null,
     };
   },
   methods: {
     getEvents: function() {
-      //  TODO: make rows=1000
+      //  TODO: make rows = 1000
       axios.get(url).then(response => {
         if (response.data.length != 0) {
-          this.events = response.data;
+          this.persons = response.data;
         } else {
           M.toast({html: "No data", classes: 'rounded'})
         };
