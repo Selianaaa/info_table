095cc4143e431dd9dd8c70515dd654cea6d5d0e7
Seliana <cany96@bk.ru>
2019-09-11 18:41:06 +0300

    table overflow-y and modal border-radius added

--- info_table.js
+++ info_table.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   });
 });
 
-const url = "http://www.filltext.com/?rows=3&id={number|1000}\
+const url = "http://www.filltext.com/?rows=20&id={number|1000}\
 &firstName={firstName}&delay=3&lastName={lastName}\
 &email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}\
 &description={lorem|32}";
