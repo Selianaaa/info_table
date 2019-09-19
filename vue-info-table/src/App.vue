6584b8ec29d6d3aabbb538ed2923bdcc1db8a919
Seliana <cany96@bk.ru>
2019-09-19 23:26:20 +0300

    App.vue file complited

--- vue-info-table/src/App.vue
+++ vue-info-table/src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">Table info</a>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="container">
+        <MainPage/>
+      </div>
+    </main>
+    <footer class="page-footer">
+      <div class="footer-copyright">
+        <div class="container">© 2019</div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
+import MainPage from './components/MainPage';
+
 export default {
   name: 'app',
+  components: {
+    MainPage,
+  },
 }
 </script>
 
 <style lang="scss">
 @import '~materialize-css/dist/css/materialize.min.css';
 @import 'assets/index.css';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- vue-info-table/src/main.js
+++ vue-info-table/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-
+import 'materialize-css/dist/js/materialize.min'
 Vue.config.productionTip = false
 
 new Vue({
